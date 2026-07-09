bea746bbe58dc7006cd574ace1a33fc55b958f83
Espermadeahtn6162 <daniel.12yt7777@gmail.com>
2026-07-08 23:20:26 -0400

    Modify global.owner and global.dev values
    Updated owner and developer information in settings.

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ import { watchFile, unwatchFile } from "fs";
 import chalk from "chalk";
 import { fileURLToPath } from "url";
 
-global.owner = ['573196588149', '50498273976'];
+global.owner = ['584121911525'];
 
 // ─── CONFIGURACIÓN AUTOMÁTICA PARA EL BOT ───
 global.pairingNumber = "584121911525"; 
 // ────────────────────────────────────────────
 
-global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜";
+global.dev = "© powered by Daxer ⚡";
 global.links = {
   api: 'https://api.yuki-wabot.my.id',
   channel: "https://whatsapp.com/channel/0029Vb64nWqLo4hb8cuxe23n",
