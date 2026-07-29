aad8de97669e043e8e75258117cb4affd51f3020
Espermadeahtn6162 <daniel.12yt7777@gmail.com>
2026-07-28 21:27:59 -0400

    Include group ID in Pong response
    Added group ID to Pong response message.

--- cmds/main/ping.js
+++ cmds/main/ping.js
@@ -6,6 +6,11 @@ export default {
     const start = Date.now()
     const sent = await sock.sendMessage(msg.chat, { text: '`❏ ¡Pong!`' + `\n> *${db.getSettings(sock.user.id.split(':')[0] + "@s.whatsapp.net").namebot}*`}, { quoted: msg })
     const latency = Date.now() - start
-    await sock.sendMessage(msg.chat, { text: `✿ *Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms`, edit: sent.key }, { quoted: msg })
+    
+    // Le agregamos la línea del ID abajo para copiarlo directo en WhatsApp
+    await sock.sendMessage(msg.chat, { 
+      text: `✿ *Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms\n\n🆔 *ID de este grupo:*\n\`\`\`${msg.chat}\`\`\``, 
+      edit: sent.key 
+    }, { quoted: msg })
   },
 };
